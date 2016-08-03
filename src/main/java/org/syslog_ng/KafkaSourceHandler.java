d4e3116803b017a15faa9e7a81a0d5053ad64f89
VIthulan <mvvithulan@gmail.com>
2016-08-03 11:57:05 +0530

    Implemented init,open, readmessage functions

--- src/main/java/org/syslog_ng/KafkaSourceHandler.java
+++ src/main/java/org/syslog_ng/KafkaSourceHandler.java
@@ -1,13 +1,33 @@
 package org.syslog_ng;
 
+import org.syslog_ng.options.Options;
+import org.syslog_ng.options.RequiredOptionDecorator;
+import org.syslog_ng.options.StringOption;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class KafkaSourceHandler extends LogSource {
+    /**
+     * Hardcoded options
+     */
+    private static final String zookeeper_host = "localhost:2181";
+    private static final String group_id_name = "groupid11";
+    private static final int threads_number = 2;
+    private static final String topic = "testtopic";
+    private static final String zookeeper_session_time_out = "400";
+    private static final String zookeeper_sync_time_out = "200";
+    private static final String commit_interval = "1000";
+
+    private KafkaProperties kafkaProperties;
+    private KafkaConsumer kafkaConsumer;
 
     public KafkaSourceHandler(long l) {
         super(l);
     }
 
     protected boolean open() {
-        return false;
+        return kafkaConsumer.createConnection();
     }
 
     protected void close() {
@@ -15,7 +35,13 @@ public class KafkaSourceHandler extends LogSource {
     }
 
     protected int readMessage(LogMessage logMessage) {
-        return 0;
+        boolean status = kafkaConsumer.poll();
+        if(status){
+            return 1;
+        }
+        else
+            return -1;
+
     }
 
     protected void ack(LogMessage logMessage) {
@@ -47,7 +73,24 @@ public class KafkaSourceHandler extends LogSource {
     }
 
     protected boolean init() {
-        return false;
+        List<String> topics = new ArrayList<String>();
+        topics.add(topic);
+
+        kafkaProperties = new KafkaProperties(zookeeper_host, group_id_name, zookeeper_session_time_out,
+                zookeeper_sync_time_out, commit_interval);
+
+        kafkaConsumer = new KafkaConsumer(kafkaProperties,topics,threads_number);
+
+        //toDo Need to change StringOptions constructor for LogSource
+       /* Options requiredOptions = new Options();
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,"zoo")));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));*/
+        return true;
     }
 
     protected void deinit() {
