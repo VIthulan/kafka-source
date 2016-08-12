a69b93459e1cc98006364bbaceb9ce5a2f174def
VIthulan <mvvithulan@gmail.com>
2016-08-12 23:54:05 +0530

    Adding options feature

--- src/main/java/org/syslog_ng/KafkaSourceHandler.java
+++ src/main/java/org/syslog_ng/KafkaSourceHandler.java
@@ -1,24 +1,16 @@
 package org.syslog_ng;
 
+import org.syslog_ng.options.InvalidOptionException;
 import org.syslog_ng.options.Options;
 import org.syslog_ng.options.RequiredOptionDecorator;
 import org.syslog_ng.options.StringOption;
 
 
 public class KafkaSourceHandler extends LogSource {
-    /**
-     * Hardcoded options
-     */
-    private static final String zookeeper_host = "localhost:2181";
-    private static final String group_id_name = "groupid11";
-    private static final int threads_number = 2;
-    private static final String topic = "testtopic";
-    private static final String zookeeper_session_time_out = "400";
-    private static final String zookeeper_sync_time_out = "200";
-    private static final String commit_interval = "1000";
     private KafkaProperties kafkaProperties;
     private KafkaConsumer kafkaConsumer;
-
+    private String topic;
+    private String group_id;
     public KafkaSourceHandler(long l) {
         super(l);
     }
@@ -50,53 +42,60 @@ public class KafkaSourceHandler extends LogSource {
 
     }
 
-    protected void ack(LogMessage logMessage) {
-
-    }
-
-    protected void nack(LogMessage logMessage) {
-
-    }
-
     protected boolean isReadable() {
         return kafkaConsumer.hasNext();
     }
     protected String getStatsInstance() {
-        return "Kafka_source_test";
+        return "Kafka_source_"+group_id+"_"+topic;
     }
 
     protected String getPersistName() {
         return null;
     }
 
-    protected String getCursor() {
-        return null;
-    }
-
-    protected boolean seekToCursor(String s) {
-        return false;
-    }
-
     protected boolean init() {
+        String zookeeper_host;
+        String group_id_name;
+        String topic;
+        String zookeeper_session_time_out;
+        String zookeeper_sync_time_out;
+        String commit_interval;
+
+        Options requiredOptions = new Options();
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.zookeeper_host)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.group_id_name)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.topic)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.zookeeper_session_time_out)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.zookeeper_sync_time_out)));
+        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.commit_interval)));
+        try {
+            requiredOptions.validate();
+        } catch (InvalidOptionException e) {
+            InternalMessageSender.error("Some options are missing");
+            return false;
+        }
+        zookeeper_host = getOption(KafkaConstants.zookeeper_host);
+        group_id_name = getOption(KafkaConstants.group_id_name);
+        topic = getOption(KafkaConstants.topic);
+        zookeeper_session_time_out = getOption(KafkaConstants.zookeeper_session_time_out);
+        zookeeper_sync_time_out = getOption(KafkaConstants.zookeeper_sync_time_out);
+        commit_interval = getOption(KafkaConstants.commit_interval);
 
         kafkaProperties = new KafkaProperties(zookeeper_host, group_id_name, zookeeper_session_time_out,
                 zookeeper_sync_time_out, commit_interval);
 
         kafkaConsumer = new KafkaConsumer(kafkaProperties,topic);
 
-        //toDo Need to change StringOptions constructor for LogSource
-       /* Options requiredOptions = new Options();
-        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,"zoo")));
-        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
-        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
-        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
-        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
-        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));
-        requiredOptions.put(new RequiredOptionDecorator(new StringOption(this,KafkaConstants.)));*/
+        this.topic = topic;
+        this.group_id = group_id_name;
         return true;
     }
 
     protected void deinit() {
-
+        try {
+            kafkaConsumer = null;
+        } catch (Exception e){
+            InternalMessageSender.error("Error occurred while deinit- "+e.getMessage());
+        }
     }
 }
